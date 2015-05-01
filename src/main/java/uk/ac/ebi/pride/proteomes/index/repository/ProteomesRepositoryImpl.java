380e4611cad14a8b2e64f909141ec4a5c6cbe5bf
FlorianEBI <florian.ebi@gmail.com>
2015-05-01 09:47:51 +0100

    update count method to use proper count of facet values rather than relying on document counts

--- src/main/java/uk/ac/ebi/pride/proteomes/index/repository/ProteomesRepositoryImpl.java
+++ src/main/java/uk/ac/ebi/pride/proteomes/index/repository/ProteomesRepositoryImpl.java
@@ -3,20 +3,13 @@ package uk.ac.ebi.pride.proteomes.index.repository;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.PageRequest;
 import org.springframework.data.solr.core.SolrTemplate;
-import org.springframework.data.solr.core.query.Criteria;
-import org.springframework.data.solr.core.query.FacetOptions;
-import org.springframework.data.solr.core.query.FacetQuery;
-import org.springframework.data.solr.core.query.SimpleFacetQuery;
-import org.springframework.data.solr.core.query.result.FacetFieldEntry;
-import org.springframework.data.solr.core.query.result.FacetPage;
-import org.springframework.data.solr.core.query.result.SolrResultPage;
+import org.springframework.data.solr.core.query.*;
+import org.springframework.data.solr.core.query.result.*;
 import org.springframework.stereotype.Repository;
 import uk.ac.ebi.pride.proteomes.index.model.PeptiForm;
 
 import javax.annotation.Resource;
-import java.util.ArrayList;
-import java.util.Collection;
-import java.util.List;
+import java.util.*;
 
 import static uk.ac.ebi.pride.proteomes.index.model.SolrPeptiForm.*;
 
@@ -100,7 +93,7 @@ public class ProteomesRepositoryImpl implements ProteomesRepositoryCustom {
         }
         // searchValues cannot be null to create a valid criteria
         if (searchValues == null) {
-            searchValues = new ArrayList<String>(1);
+            searchValues = new ArrayList<String>(0);
             // not needed to set a default value,
             // empty list = match all
         }
@@ -110,12 +103,37 @@ public class ProteomesRepositoryImpl implements ProteomesRepositoryCustom {
         facetQuery.setPageRequest(new PageRequest(0, 1));
 
         FacetPage<PeptiForm> facetPage = solrTemplate.queryForFacetPage(facetQuery, PeptiForm.class);
+
+        FacetOptions options2 = new FacetOptions(facetField);
+        options2.setFacetLimit(10000000);
+        FacetQuery facetQuery2 = facetQuery.setFacetOptions(options2);
+        FacetPage<PeptiForm> facetPage2 = solrTemplate.queryForFacetPage(facetQuery2, PeptiForm.class);
+
+        long totalCount = facetPage2.getFacetResultPage(facetField).getContent().size();
+
+
+        // stats version
+        StatsOptions statsOptions = new StatsOptions()
+                .addField(facetField)
+                .setSelectiveCalcDistinct(true)
+                .setCalcDistinct(true);
+
+
+        SimpleQuery statsQuery = new SimpleQuery(criteria);
+        statsQuery.setStatsOptions(statsOptions);
+        StatsPage<PeptiForm> statsPage = solrTemplate.queryForStatsPage(statsQuery, PeptiForm.class);
+        FieldStatsResult priceStatResult = statsPage.getFieldStatsResult(facetField);
+        long totalCountStats = priceStatResult.getDistinctCount();
+
+
         // we don't want to return the full facet page, so we extract the bits we want
         Page<FacetFieldEntry> tempPage = facetPage.getFacetResultPage(facetField);
         PageRequest pageRequest = new PageRequest(tempPage.getNumber(), tempPage.getSize(), tempPage.getSort());
         // on the result page set the total count of the initial result, not of the paged facet part
         // ToDo: facetPage.getTotalElements is NOT correct, as it counts the documents, not the unique facet values
-        return new SolrResultPage<FacetFieldEntry>(tempPage.getContent(), pageRequest, facetPage.getTotalElements(), 0.0f);
+//        return new SolrResultPage<FacetFieldEntry>(tempPage.getContent(), pageRequest, tempPage.getTotalElements(), 0.0f);
+        return new SolrResultPage<FacetFieldEntry>(tempPage.getContent(), pageRequest, totalCount, 0.0f);
     }
+
 
 }
